65f606ad701406a37ac7e1fa737e4428243c51e4
Mehdi-Zen <mbelayachi@campus-cci84.fr>
2025-10-23 14:42:23 +0200

    ETAPE 9 REFACTOR

--- 3-fizzbuzz/src/fizzbuzz.js
+++ 3-fizzbuzz/src/fizzbuzz.js
@@ -1,14 +1,10 @@
 function fizzBuzz(number) {
- if (number % 15 === 0) {
- return 'FizzBuzz';
- }
- if (number % 3 === 0) {
- return 'Fizz';
- }
- if (number % 5 === 0) {
- return 'Buzz';
- }
- return String(number);
+ let result = '';
+
+ if (number % 3 === 0) result += 'Fizz';
+ if (number % 5 === 0) result += 'Buzz';
+
+ return result || String(number);
 }
 
 module.exports = fizzBuzz;
