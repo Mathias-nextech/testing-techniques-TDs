d9be5c1ba8c952514d6307991a9692abaafee627
Mehdi-Zen <mbelayachi@campus-cci84.fr>
2025-10-23 14:40:27 +0200

    -ETAPE 5 RED (BUZZ)

--- 3-fizzbuzz/specs/fizzbuzz.spec.js
+++ 3-fizzbuzz/specs/fizzbuzz.spec.js
@@ -12,3 +12,8 @@ it('should return "Fizz" for multiples of 3', () => {
  expect(fizzBuzz(6)).toBe('Fizz');
  expect(fizzBuzz(9)).toBe('Fizz');
 });
+
+it('should return "Buzz" for multiples of 5', () => {
+ expect(fizzBuzz(5)).toBe('Buzz');
+ expect(fizzBuzz(10)).toBe('Buzz');
+});
